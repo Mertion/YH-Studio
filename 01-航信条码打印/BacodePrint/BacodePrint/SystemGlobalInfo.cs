63a02b8ab694b72389d85efba6d9c81e55d7ef76
Mertion <li_zhiguoxq@163.com>
2023-04-22 11:58:03 +0800

    调整程序目录

--- 01-航信条码打印/BacodePrint/BacodePrint/SystemGlobalInfo.cs
+++ 01-航信条码打印/BacodePrint/BacodePrint/SystemGlobalInfo.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace BacodePrint
 {
-
+    //系统参数类，使用单例模式
     public sealed class SystemGlobalInfo
     {
         private static readonly SystemGlobalInfo instance = new SystemGlobalInfo();
         private static object obj = new object();
 
+        //配置文件路径
         public string mstrConfigFilePath = Convert.ToString(System.AppDomain.CurrentDomain.BaseDirectory) + "Config.ini";
+
+        List<UserControlTextBoxItems> listText = new List<UserControlTextBoxItems>();
+        public const int nListMaxCount = 34;
+
         /// <summary>
         /// 显式的静态构造函数用来告诉C#编译器在其内容实例化之前不要标记其类型
         /// </summary>
